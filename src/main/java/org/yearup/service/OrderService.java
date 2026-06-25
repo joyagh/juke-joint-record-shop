c244d0c564966c450cfa5a9ca66ee1c22c078b48
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-06-25 11:53:25 -0500

    completed order service

--- src/main/java/org/yearup/service/OrderService.java
+++ src/main/java/org/yearup/service/OrderService.java
@@ -1,4 +1,74 @@
 package org.yearup.service;
 
-public class OrderService {
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.yearup.models.*;
+import org.yearup.repository.OrderLineItemRepository;
+import org.yearup.repository.OrderRepository;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class OrderService
+{
+    private final OrderRepository orderRepository;
+    private final OrderLineItemRepository orderLineItemRepository;
+    private final ShoppingCartService shoppingCartService;
+    private final UserService userService;
+    private final ProfileService profileService;
+
+    public OrderService(OrderRepository orderRepository,
+                        OrderLineItemRepository orderLineItemRepository,
+                        ShoppingCartService shoppingCartService,
+                        UserService userService,
+                        ProfileService profileService)
+    {
+        this.orderRepository = orderRepository;
+        this.orderLineItemRepository = orderLineItemRepository;
+        this.shoppingCartService = shoppingCartService;
+        this.userService = userService;
+        this.profileService = profileService;
+    }
+
+    @Transactional
+    public Order checkout(int userId)
+    {
+        // get  user profile for address info
+        Profile profile = profileService.getByUserId(userId);
+
+        // create  order
+        Order order = new Order();
+        order.setUserId(userId);
+        order.setDate(LocalDateTime.now());
+        order.setAddress(profile.getAddress());
+        order.setCity(profile.getCity());
+        order.setState(profile.getState());
+        order.setZip(profile.getZip());
+        order.setShippingAmount(BigDecimal.ZERO);
+
+        // save the order to get the  order id
+        Order savedOrder = orderRepository.save(order);
+
+        // get the user's cart
+        ShoppingCart cart = shoppingCartService.getByUserId(userId);
+
+        // create a line item for each product in the cart
+        for (ShoppingCartItem item : cart.getItems().values())
+        {
+            OrderLineItem lineItem = new OrderLineItem();
+            lineItem.setOrderId(savedOrder.getOrderId());
+            lineItem.setProductId(item.getProduct().getProductId());
+            lineItem.setSalesPrice(BigDecimal.valueOf(item.getProduct().getPrice()));
+            lineItem.setQuantity(item.getQuantity());
+            lineItem.setDiscount(BigDecimal.ZERO);
+            orderLineItemRepository.save(lineItem);
+        }
+
+        // clear cart
+        shoppingCartService.clearCart(userId);
+
+        return savedOrder;
+    }
 }
