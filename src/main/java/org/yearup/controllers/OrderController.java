e3845a32912bc0a9c6fc5775ae93fe57d89b1893
Justine Oyaghiro <justineoyaghiro@icloud.com>
2026-06-25 13:56:52 -0500

    completed order controller. finished phase 5

--- src/main/java/org/yearup/controllers/OrderController.java
+++ src/main/java/org/yearup/controllers/OrderController.java
@@ -1,4 +1,41 @@
 package org.yearup.controllers;
 
-public class OrderController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.yearup.models.Order;
+import org.yearup.models.User;
+import org.yearup.service.OrderService;
+import org.yearup.service.UserService;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("orders")
+@CrossOrigin
+@PreAuthorize("isAuthenticated()")
+public class OrderController
+{
+    private final OrderService orderService;
+    private final UserService userService;
+
+    @Autowired
+    public OrderController(OrderService orderService, UserService userService)
+    {
+        this.orderService = orderService;
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Order> checkout(Principal principal)
+    {
+        String userName = principal.getName();
+        User user = userService.getByUserName(userName);
+        int userId = user.getId();
+
+        Order order = orderService.checkout(userId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(order);
+    }
 }
